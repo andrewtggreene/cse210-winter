e83b945108bc89c19a140a7ff3c9647291f3e021
Your Name <you@example.com>
2024-01-09 23:28:51 -0700

    Completed the Grade Conditional Project Plus Stretch Challenges.

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,45 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("What is your grade percentage? ");
+        string gradeString = Console.ReadLine();
+        int gradePercent = int.Parse(gradeString);
+        string letterGrade;
+        string plusMinus = "";
+        if (gradePercent >= 90) {
+            letterGrade = "A";
+            if (gradePercent < 93) {
+                plusMinus = "-";
+            }
+        } else if (gradePercent >= 80) {
+            letterGrade = "B";
+            if (gradePercent >= 87) {
+                plusMinus = "+";
+            } else if (gradePercent < 83) {
+                plusMinus = "-";
+            }
+        } else if (gradePercent >= 70) {
+            letterGrade = "C";
+            if (gradePercent >= 77) {
+                plusMinus = "+";
+            } else if (gradePercent < 73) {
+                plusMinus = "-";
+            }
+        } else if (gradePercent >= 60) {
+            letterGrade = "D";
+            if (gradePercent >= 67) {
+                plusMinus = "+";
+            } else if (gradePercent < 63) {
+                plusMinus = "-";
+            }
+        } else {
+            letterGrade = "F";
+        }
+        Console.WriteLine($"Your grade is a {letterGrade}{plusMinus}");
+        if (gradePercent >= 70) {
+            Console.WriteLine("Congratulations! You have passed the class!");
+        } else {
+            Console.WriteLine("Bummer! Try Harder Next Time, You Failed.");
+        }
     }
 }
