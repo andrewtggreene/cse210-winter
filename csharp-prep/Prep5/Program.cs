665c914e0dcc0f0aa1c8c7ac60cc2bafde2d911c
andrewtggreene <mathtron@gmail.com>
2024-01-12 18:08:04 -0700

    Completed prep 5 assignment

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -1,9 +1,40 @@
 using System;
+using System.Globalization;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep5 World!");
+        DisplayWelcome();
+        DisplayResult();
+    }
+    static void DisplayWelcome()
+    {
+        Console.WriteLine("Welcome to the program!");
+    }
+    static string PromptUserName()
+    {
+        Console.Write("Please enter your name: ");
+        string userName = Console.ReadLine();
+        return userName;
+    }
+    static int PromptUserNumber()
+    {
+        Console.Write("Please enter your facorite number: ");
+        string numberString = Console.ReadLine();
+        int number = int.Parse(numberString);
+        return number;
+    }
+    static int SquareNumber(int number)
+    {
+        int newNumber = number * number;
+        return newNumber;
+    }
+    static void DisplayResult()
+    {
+        string name = PromptUserName();
+        int number = PromptUserNumber();
+        int numberSquared = SquareNumber(number);
+        Console.WriteLine($"{name}, the square of your number is {numberSquared}");
     }
 }
